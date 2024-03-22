2c8be6017baba19ea023ad1315267bf293cb18ff
Kwaku Appenteng Wiredu <kwakuwiredu0@gmail.com>
2024-03-22 03:17:41 -0700

    back to start

--- HBG/settings.py
+++ HBG/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3saygep+!4ah7g50ucgfg@c=l7ks8wtm4@k2__pxd@ao1&+s6l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
